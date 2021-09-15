f8e6f641ebf12e89b0c916d6126bfa6aa36bb098
Flavio Rocha <flavio_271295@hotmail.com>
2021-09-15 11:11:31 -0300

    revert pages name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-import { Routes } from "./pagess/Routes";
+import { Routes } from "./pages/Routes";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
