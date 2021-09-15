5ec99136fbd06e6727ef299f8bbdf96fd848c24d
Flavio Rocha <flavio_271295@hotmail.com>
2021-09-15 11:09:24 -0300

    change folder pages name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-import { Routes } from "./pages/Routes";
+import { Routes } from "./pagess/Routes";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
