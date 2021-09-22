5902f2e01cbc45501e7fcce8dcfa7672febe1cbf
Flavio Rocha <flavio_271295@hotmail.com>
2021-09-22 14:53:13 -0300

    test deploy

--- src/pages/WriteGame.jsx
+++ src/pages/WriteGame.jsx
@@ -49,6 +49,7 @@ function WriteGame() {
 
   useEffect(() => {
     console.log(process.env.REACT_APP_HOST)
+    console.log(process.env.REACT_APP_HOST)
 
     audio.addEventListener(
       "ended",
